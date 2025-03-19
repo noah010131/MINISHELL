702bca3c4a266857647ccae9f0299bc90365f5f0
Chanyoung Park <chanypar@paul-f4Br8s1.clusters.42paris.fr>
2025-03-19 21:07:27 +0100

    cd modify

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -3,43 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jihyeki2 <jihyeki2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chanypar <chanypar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 21:44:17 by ihibti            #+#    #+#             */
-/*   Updated: 2025/03/19 20:32:47 by jihyeki2         ###   ########.fr       */
+/*   Updated: 2025/03/19 21:02:16 by chanypar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// int	ft_cd(t_pars *pars, t_envp **lst)
-// {
-// 	int		i;
-// 	char	**dict;
-// 	char	new_pwd[2048];
+int	cd_helper(t_envp **lst)
+{
+	char	new_pwd[2048];
 
-// 	i = 0;
-// 	if (!pars)
-// 		return (ft_putstr_fd("erreur cd\n", 2), 1);
-// 	dict = pars->arguments;
-// 	while (dict[i])
-// 		i++;
-// 	if (i != 2)
-// 		return (ft_putstr_fd("cd : too many arguments\n", 2), 1);
-// 	if (chdir(dict[1]) == -1)
-// 		return (ft_putstr_fd("cd : No such file or directory\n", 2), 1);
-// 	if (!getcwd(new_pwd, 2048))
-// 		return (1);
-// 	update_env(lst, "PWD", new_pwd);
-// 	return (0);
-// }
+	if (!getcwd(new_pwd, 2048))
+		return (1);
+	update_env(lst, "PWD", new_pwd);
+	return (0);
+}
 
 int	ft_cd(t_ori *ori, t_pars *pars)
 {
 	int		i;
 	t_envp	*home;
 	t_envp	**lst;
-	char	new_pwd[2048];
 
 	lst = ori->envs;
 	i = 0;
@@ -59,8 +46,5 @@ int	ft_cd(t_ori *ori, t_pars *pars)
 		return (ft_putstr_fd("cd : too many arguments\n", 2), 1);
 	else if (chdir(pars->arguments[1]) == -1)
 		return (ft_putstr_fd("cd : No such file or directory\n", 2), 1);
-	if (!getcwd(new_pwd, 2048))
-		return (1);
-	update_env(lst, "PWD", new_pwd);
-	return (0);
+	return (cd_helper(lst));
 }
